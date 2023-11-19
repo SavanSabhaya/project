d58403591d9be867729c484fbc95e1c2191de50b
Bhavin Sabhaya <sabhayabhavin88@gmail.com>
2023-11-20 04:22:27 +0530

    first commit

---
+++ build/app/generated/source/buildConfig/release/com/myapp/pmck/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.myapp.pmck;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.myapp.pmck";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 55;
+  public static final String VERSION_NAME = "2.43.1";
+}
